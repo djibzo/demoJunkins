afbc701ca6d39a519d7eb5dec6bbd05d780d29ca
djibzo <djibzo90@gmail.com>
2024-01-27 02:08:12 +0000

    ajout de la ligne 10 affichage de ok

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ok");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
-
 }
