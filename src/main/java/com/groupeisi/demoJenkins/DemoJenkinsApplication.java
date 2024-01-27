c820b3faae652d40cd05be3065c0d77c6591a42b
djibzo <djibzo90@gmail.com>
2024-01-27 02:24:05 +0000

    test4

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -10,6 +10,7 @@ public class DemoJenkinsApplication {
 		System.out.println("ok");
 		System.out.println("test2 du l'automatisation");
 		System.out.println("test3 du l'automatisation");
+		System.out.println("test4 du l'automatisation");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 }
