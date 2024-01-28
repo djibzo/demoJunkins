d2af29f55846391e174e43ede2deba8805188dd8
djibzo <djibzo90@gmail.com>
2024-01-28 00:26:10 +0000

    welcome message

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ok");
+		System.out.println("Welcome ");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 }
